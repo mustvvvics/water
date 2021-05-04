2cbac8950b6c0a1cfa0770b2b9fd59b32746aa68
mustvvvics <mustvvvics@gmail.com>
2021-05-04 20:59:46 +0800

    added multiprocessing

--- allnew.py
+++ allnew.py
@@ -5,6 +5,7 @@ Created on Tue Apr 20 20:22:49 2021
 @author: Mustvvvics
 """
 import time,threading
+from multiprocessing import Process, Value
 from time import sleep
 from queue import Queue                         # q = Queue()
 import cv2
@@ -19,12 +20,12 @@ import requests as r
 
 
 # target#################################################################
-targetHeight = 9
-targetTemperature = 30
+targetHeight = Value('f', 9)
+targetTemperature = Value('f', 30)
 temperatureFlag = 0                             # 0:heating 1:drop temperature 
 
 # Initial fun pid ########################################################
-Sv = targetTemperature * 10
+Sv = targetTemperature.value * 10
 Pv = 350
 
 Kp = 1
@@ -41,10 +42,10 @@ changeylist = []
 i = 0
 y = 0
 # Initial Value##########################################################
-height = 0
-temperature = 0
-errorHeight = 0
-errorTemperature = 0
+height = Value('f', 0)
+temperature = Value('f', 0)
+errorHeight = Value('f', 0)
+errorTemperature = Value('f', 0)
 
 # Initial Temperature####################################################
 file = "/dev/hidraw0"
@@ -67,28 +68,6 @@ IN2 = 15                                        # site GPIO15 link IN2
 
 freq = 500                                      # PWM frequency
 speed = 10                                      # PWM Initialising speed
-
-# Initial Camera Value###################################################
-CV_CAP_PROP_FRAME_WIDTH = 3
-CV_CAP_PROP_FRAME_HEIGHT = 4
-CV_CAP_PROP_FPS = 5
-CV_CAP_PROP_FOURCC = 6
-
-name = 0
-cap = cv2.VideoCapture(name)                    # Turn on the built-in camera
-
-cap.set(CV_CAP_PROP_FPS, 1)
-
-frameWidth = 640
-frameHeight = 480
-
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, frameWidth)   # Setup form 1920 * 1080 
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, frameHeight)
-
-ratioWindowsHeight = 1                          # Form transformation
-ratioWindowsWidth = 0.6
-windowsWidth = int(frameWidth * ( 0.5 - ratioWindowsWidth / 2 )), int(frameWidth * ( 0.5 + ratioWindowsWidth / 2 ))
-windowsHeight = int(frameHeight * ( 0.5 - ratioWindowsHeight / 2 )), int(frameHeight * ( 0.5 + ratioWindowsHeight / 2 ))
 
 # Flag###################################################################
 flagWorking = True
@@ -153,13 +132,13 @@ def controlHeight():
     waterDeath = 0.08
 
     while flagWorking:
-        if errorHeight >= waterDeath :
+        if errorHeight.value >= waterDeath :
             pumpWater()
             pwm.ChangeDutyCycle(100)
-        elif errorHeight <= -waterDeath :
+        elif errorHeight.value <= -waterDeath :
             releaseWater()
             pwm.ChangeDutyCycle(100)
-        elif abs(errorHeight) < waterDeath :
+        elif abs(errorHeight.value) < waterDeath :
             pwm.ChangeDutyCycle(0)
 
 def controlTemperature():
@@ -173,19 +152,17 @@ def controlTemperature():
     while flagWorking:
         # Get Temperature
         fp = open(file,'rb')            
-        temperature = fp.read(4)
-        temperature = temperature[2:]
-        temperature = int.from_bytes(temperature, byteorder="big") 
-        funTemperature = temperature
-        temperature /= 10                   # e.g 301°C to 30.1°C
+        temperature.value = int.from_bytes(fp.read(4)[2:], byteorder='big')
+        funTemperature = temperature.value
+        temperature.value /= 10                   # e.g 301°C to 30.1°C
         # Heating to 30 needs to be advanced 0.5°C, heating to 35 needs to be advanced 0°C
-        errorTemperature = targetTemperature - temperature 
+        errorTemperature.value = targetTemperature.value - temperature.value 
 
         advancedTemperature = 0.5
         if temperatureFlag == 0:            #Heating
-            if (errorTemperature - advancedTemperature) > 0.2:      # Working
+            if (errorTemperature.value - advancedTemperature) > 0.2:      # Working
                 GPIO.output(RelayPin, GPIO.LOW)
-            if (errorTemperature - advancedTemperature) <= 0.2:     # Close
+            if (errorTemperature.value - advancedTemperature) <= 0.2:     # Close
                 GPIO.output(RelayPin, GPIO.HIGH)
         elif temperatureFlag == 1:
             pwmfun.start(funSpeed) 
@@ -217,7 +194,7 @@ def controlTemperature():
             # print("temperature",funTemperature) 
             pwmfun.ChangeDutyCycle(yappend)     
             changeylist.append(yappend) 
-            if temperature == Sv:
+            if temperature.value == Sv:
                 # plt.show()
                 flag = 0
                 Ek = 0
@@ -229,8 +206,63 @@ def controlTemperature():
                 Dout = 0
                 i = 0
 
-def controlProcess():
-    pass
+def cameraProcess(height, errorHeight, targetHeight):
+    # Initial Camera Value###################################################
+    CV_CAP_PROP_FRAME_WIDTH = 3
+    CV_CAP_PROP_FRAME_HEIGHT = 4
+    CV_CAP_PROP_FPS = 5
+    CV_CAP_PROP_FOURCC = 6
+
+    name = 0
+    cap = cv2.VideoCapture(name)                    # Turn on the built-in camera
+
+    cap.set(CV_CAP_PROP_FPS, 1)
+
+    frameWidth = 640
+    frameHeight = 480
+
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, frameWidth)   # Setup form 1920 * 1080 
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, frameHeight)
+
+    ratioWindowsHeight = 1                          # Form transformation
+    ratioWindowsWidth = 0.6
+    windowsWidth = int(frameWidth * ( 0.5 - ratioWindowsWidth / 2 )), int(frameWidth * ( 0.5 + ratioWindowsWidth / 2 ))
+    windowsHeight = int(frameHeight * ( 0.5 - ratioWindowsHeight / 2 )), int(frameHeight * ( 0.5 + ratioWindowsHeight / 2 ))
+
+    while True:
+        ret,img = cap.read()        #Read the image in real time
+        img = img[ windowsHeight[0] : windowsHeight[1], windowsWidth[0] : windowsWidth[1]]
+
+        imggray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+        ret, thresh = cv2.threshold(imggray,50,255,0)
+        _, contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+        
+        for cnt in contours:
+            if (cv2.contourArea(cnt) > 3000) and (cv2.contourArea(cnt) < 52000):
+                # draw a rectangle around the items
+                clear() #清屏 
+                print(cv2.contourArea(cnt))
+                x,y,w,h = cv2.boundingRect(cnt)
+                cv2.rectangle(img, (x,y), (x+w,y+h), (0,255,0),3)
+                print("height", h)
+                height.value = CalculationHigh(h)
+                errorHeight.value = targetHeight.value - height.value
+
+        print("目标液位",targetHeight.value,"\t目标温度",targetTemperature.value)
+        print("当前液位",round(height.value, 2), "\t当前温度",round(temperature.value, 1))
+        print("误差液位",round(errorHeight.value, 2),"\t误差温度",round(errorTemperature.value, 1))
+
+        cv2.imshow("Camera", img)
+        
+        k = cv2.waitKey(1)
+        if (k == ord('q')):
+            break
+        elif(k == ord('s')):        # Take Photo
+            name += 1
+            filename = '/home/pi/sheji/' + str(name) + 'test' + '.jpg'
+            cv2.imwrite(filename, img)
+            break 
+    cv2.destroyAllWindows()
 
 def dealTcpData(jsonData):
     global targetHeight
@@ -239,13 +271,13 @@ def dealTcpData(jsonData):
     command = jsonData['data']
     print(command)
     if (command == "higher"):
-        targetHeight += 2
+        targetHeight.value += 2
     elif (command == "lower"):
-        targetHeight -= 2
+        targetHeight.value -= 2
     elif (command == "hotter"):
-        targetTemperature += 1
+        targetTemperature.value += 1
     elif (command == "cooler"):
-        targetTemperature -= 1
+        targetTemperature.value -= 1
     else:
         return "command error"
     return "con ok"
@@ -290,10 +322,10 @@ def makeUpdate():
         time.sleep(1)
         msg = {
             'mode': 'u',
-            't': temperature,
-            'l': round(height, 1),
-            'dt': targetTemperature,
-            'dl': targetHeight,
+            't': round(temperature.value, 1),
+            'l': round(height.value, 1),
+            'dt': round(targetTemperature.value, 1),
+            'dl': round(targetHeight.value, 1),
         }
         try:
             echo = r.post(url, data = msg)
@@ -309,54 +341,17 @@ if __name__ == '__main__':
     tServer = threading.Thread(target = tcpServer)
     tUpdate = threading.Thread(target = makeUpdate)
 
+    pCamera = Process(target = cameraProcess, args=(height, errorHeight, targetHeight))
+
     t1.start()
     t2.start()
 
     tServer.start()
     tUpdate.start()
 
-    try: 
-        while(1):
+    pCamera.start()
+    pCamera.join()
 
-            ret,img = cap.read()        #Read the image in real time
-            img = img[ windowsHeight[0] : windowsHeight[1], windowsWidth[0] : windowsWidth[1]]
-
-            imggray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-            ret, thresh = cv2.threshold(imggray,50,255,0)
-            _, contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-            
-            for cnt in contours:
-                if (cv2.contourArea(cnt) > 3000) and (cv2.contourArea(cnt) < 52000):
-                    # draw a rectangle around the items
-                    clear() #清屏 
-                    print(cv2.contourArea(cnt))
-                    x,y,w,h = cv2.boundingRect(cnt)
-                    cv2.rectangle(img, (x,y), (x+w,y+h), (0,255,0),3)
-                    print(h)
-                    height = CalculationHigh(h)
-                    errorHeight = targetHeight - height
-                            
-            print("目标液位",targetHeight,"\t目标温度",targetTemperature)
-            print("当前液位",round(height, 2), "\t当前温度",temperature)
-            print("误差液位",round(errorHeight, 2),"\t误差温度",round(errorTemperature, 2))
-
-            cv2.imshow("-1", img)
-            
-            k = cv2.waitKey(20)
-            if (k == ord('q')):
-                break
-            elif(k == ord('s')):        # Take Photo
-                name += 1
-                filename = '/home/pi/sheji/' + str(name) + 'test' + '.jpg'
-                cv2.imwrite(filename, img)
-                break 
-    except KeyboardInterrupt:
-        print("ancled program.")
-    except:
-        print("some error.")
-        raise
-    finally:       
-        print("clean all.")
-        flagWorking = False
-        gpioDestroy()
-        cv2.destroyAllWindows()
+    print("clean all.")
+    flagWorking = False
+    gpioDestroy()
